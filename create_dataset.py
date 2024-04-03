747b0c48c8b18b8d55248381218cb02f8495211c
apoupon <81863563+apoupon@users.noreply.github.com>
2024-04-03 15:29:58 +0200

    numerical values for polarities

--- create_dataset.py
+++ create_dataset.py
@@ -14,9 +14,21 @@ pd.set_option("display.max_colwidth", None)
 for row in df_train.itertuples():
     df_train.at[row.Index, 'pseudo_sentence'] = row.term + ' ' + row.aspect
     df_train = df_train[['sentence', 'pseudo_sentence', 'polarity']]
+    if row.polarity == 'negative':
+        df_train.at[row.Index, 'polarity'] = 0
+    elif row.polarity == 'neutral':
+        df_train.at[row.Index, 'polarity'] = 1
+    elif row.polarity == 'positive':
+        df_train.at[row.Index, 'polarity'] = 2
 for row in df_dev.itertuples():
     df_dev.at[row.Index, 'pseudo_sentence'] = row.term + ' ' + row.aspect
     df_dev = df_dev[['sentence', 'pseudo_sentence', 'polarity']]
+    if row.polarity == 'negative':
+        df_train.at[row.Index, 'polarity'] = 0
+    elif row.polarity == 'neutral':
+        df_train.at[row.Index, 'polarity'] = 1
+    elif row.polarity == 'positive':
+        df_train.at[row.Index, 'polarity'] = 2
 
 df_train = df_train.rename(columns={'polarity': 'label'})
 df_dev = df_dev.rename(columns={'polarity': 'label'})
@@ -27,3 +39,9 @@ dev_dataset = datasets.Dataset.from_pandas(df_dev)
 
 # combine the train and dev datasets
 sft_dataset = datasets.DatasetDict({'train': train_dataset, 'validation': dev_dataset})
+
+if __name__ == '__main__':
+    print('sft_dataset')
+    print(sft_dataset)
+    print('label values')
+    print(sft_dataset['train']['label'][:10])
