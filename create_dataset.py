32ef03ee0d9a45e802b9b74726a4aa2bca5f271b
apoupon <81863563+apoupon@users.noreply.github.com>
2024-04-03 15:21:40 +0200

    script for dataset creation

---
+++ create_dataset.py
@@ -0,0 +1,29 @@
+# Import
+import transformers
+import tokenizers
+import datasets
+import pandas as pd
+import numpy as np
+
+# load csv files
+df_train = pd.read_csv('data/traindata.csv', delimiter='\t', names=['polarity', 'aspect', 'term', 'term_pos', 'sentence'])
+df_dev = pd.read_csv('data/devdata.csv', delimiter='\t', names=['polarity', 'aspect', 'term', 'term_pos', 'sentence'])
+pd.set_option("display.max_colwidth", None)
+
+# create a new data frame with 3 features: sentence, pseudo-sentence (<term> <aspect>) and label (polarity)
+for row in df_train.itertuples():
+    df_train.at[row.Index, 'pseudo_sentence'] = row.term + ' ' + row.aspect
+    df_train = df_train[['sentence', 'pseudo_sentence', 'polarity']]
+for row in df_dev.itertuples():
+    df_dev.at[row.Index, 'pseudo_sentence'] = row.term + ' ' + row.aspect
+    df_dev = df_dev[['sentence', 'pseudo_sentence', 'polarity']]
+
+df_train = df_train.rename(columns={'polarity': 'label'})
+df_dev = df_dev.rename(columns={'polarity': 'label'})
+
+# convert the data frame to a Dataset
+train_dataset = datasets.Dataset.from_pandas(df_train)
+dev_dataset = datasets.Dataset.from_pandas(df_dev)
+
+# combine the train and dev datasets
+sft_dataset = datasets.DatasetDict({'train': train_dataset, 'validation': dev_dataset})
